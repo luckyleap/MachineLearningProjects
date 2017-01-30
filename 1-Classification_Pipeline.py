202dc4a0936d9818c9127516484a27671a85d350
luckyleap <david.r.tsui@gmail.com>
2017-01-30 17:17:13 -0500

    Finish training testing for SVM on part 1

--- 1-Classification_Pipeline.py
+++ 1-Classification_Pipeline.py
@@ -9,7 +9,7 @@ for line in file:
 	line = line.strip()
 	array = line.split(',')
 	data.append(array[0:-1])
-	ans.append(array[-1]);
+	ans.append(array[-1])
 
 
 file.close()
@@ -23,8 +23,28 @@ file.close()
 [trainData, testData] = data_preprocess.split_data(data, 0.6)
 [trainAns, testAns] = data_preprocess.split_data(ans, 0.6)
 
-[trainData, validData] = data_preprocess.split_data(trainData, (2.0/3.0))
-[trainAns, validAns] = data_preprocess.split_data(trainAns, (2.0/3.0))
+[trainDataSmall, validData] = data_preprocess.split_data(trainData, (2.0 / 3.0))
+[trainAnsSmall, validAns] = data_preprocess.split_data(trainAns, (2.0 / 3.0))
 
+# Iterates through all possible combinations of parameters
+C_Param_Values = [1, 50, 200, 500, 1000]
+accuracy_a = []
 
+for param in C_Param_Values:
+	clf = svm.SVC(C=param)
+	clf.fit(trainDataSmall, trainAnsSmall)
+	accuracy = clf.score(validData, validAns, sample_weight=None)
+	accuracy_a.append(accuracy)
+
+param_value = C_Param_Values[data_preprocess.findIndex(accuracy_a, max(accuracy_a))[0]]
+print param_value
+
+# Run actual param on training
+clf = svm.SVC(C=param_value)
+clf.fit(trainData, trainAns)
+train_error = clf.score(trainData, trainAns)
+test_error = clf.score(testData, testAns)
+
+generalization_error = train_error - test_error
+print(generalization_error)
 
--- data_preprocess.py
+++ data_preprocess.py
@@ -1,18 +1,40 @@
 import random
 
-def shuffle( array, ans ):
+
+def shuffle(array, ans):
 	c = list(zip(array, ans))
 	random.shuffle(c)
 	array, ans = zip(*c)
 
+	array = cleanArray(array)
+	ans = cleanArray(ans)
 	return [array, ans]
 
-def split_data( data, percent ):
-	l = len(data)
-	train_l = int(round(l * percent))
 
+def cleanArray(array):
+	ans = []
+	for data in array:
+		if len(data) != 0:
+			ans.append(data)
+
+	return ans
+
+
+def split_data(data, percent):
+	length = len(data)
+	train_l = int(round(length * percent))
 
 	train = data[0:train_l]
 	test = data[train_l:]
 
 	return [train, test]
+
+
+# Returns array of indexes of all matching values
+def findIndex(data, value):
+	ans = []
+	for i in range(0, len(data)):
+		if data[i] == value:
+			ans.append(i)
+
+	return ans
